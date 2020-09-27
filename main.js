5eed4022376a9f5e29682e2b3ab4ca45d7b23167
Tetiank-a <58983291+Tetiank-a@users.noreply.github.com>
2020-09-27 20:57:43 +0300

    Test
    Created Test.html

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 });
 //NORMAL JS
 function changeImage1(img1, img2, picId) {
-    s=picId;
+    s = picId;
     if (document.images[s].alt == "Letter") {
         document.images[s].src = img2;
         document.images[s].alt = "Image";
@@ -27,8 +27,37 @@ function changeImage1(img1, img2, picId) {
 function Check() {
     alert("Hi");
 }
-function pl(a)
-{
+function pl(a) {
     var snd = new Audio(a);
     snd.play();
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+var x = 0, y=0;
+function myFunction()
+{
+    document.getElementById('score').innerHTML = "---";
+}
+function starttest(k) {
+    y++;
+    var s = ["Aligator", "Elephant", "Hippo", "Tiger", "Bird", "Monkey", "Kenguru"];
+    if (k == document.getElementById('Animal').textContent) {
+        //alert("YES");
+        document.getElementById('Animal').innerHTML = s[getRandomInt(7)];
+        x++;
+        if (y == 8)
+        {
+            alert("YOUR SCORE IS " + x + " / 8");
+            y=0;
+            x=0;
+        }
+        document.getElementById('score').innerHTML = "CORRECT";
+        setTimeout(myFunction, 1000);
+    }
+    else {
+        //alert("NO");
+        document.getElementById('score').innerHTML = "TRY AGAIN";
+        setTimeout(myFunction, 1000);
+    }
 }
